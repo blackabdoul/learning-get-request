c9991474641ae6928b6321eb37afef0fd5ad4012
Abdoul <abdulmaliklawal12345@gmail.com>
2025-12-10 08:41:22 +0000

    this commits the main file

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send(
+    "<h1>About Me</h1><p>My name is Abdoul</p><p>I'm exited to be currently in my back-end journey</p>"
+  );
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Me</h1><p>Phone: +1234567890</p>");
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
